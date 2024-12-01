b9877c62b489acf4687f10e51875004f78364769
MrReva <azadit441@gmail.com>
2024-12-01 11:24:25 +0330

    fix:display menu issue + add minor changes

--- installer.sh
+++ installer.sh
@@ -19,12 +19,15 @@ fi
 
 echo "Setting up execute permissions for scripts..."
 chmod +x scripts/create_container.sh 
-
+chmod +x scripts/manage_containers.sh
+chmod +x scripts/menu.sh
+chmod +x scripts/resource_report.sh
+chmod +x scripts/git_deploy.sh
 echo "Running create_container.sh..."
-./scripts/create_container.sh 
+./scripts/menu.sh 
 
 echo "Creating a shortcut for 'DevOps-Kit' command..."
-sudo ln -s $(pwd)/scripts/create_container.sh /usr/local/bin/DevOps-Kit
+sudo ln -s $(pwd)/scripts/menu.sh /usr/local/bin/DevOps-Kit
 
 echo "Installation complete! Your DevOps Kit is ready to use."
 echo "You can now use the 'DevOps-Kit' command from anywhere to run the create_container.sh script."
---
+++ scripts/menu.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+show_menu() {
+
+  echo -e "\e[35m10) =| CreateContainer\e[0m"
+  echo -e "\e[36m20) =| ConnectToGitRepo\e[0m"
+  echo -e "\e[36m11) =| ManageContainer\e[0m"
+  echo -e "\e[33m12) =| ContainerUsageReport\e[0m"
+}
+
+create_container() {
+  echo "Please select a platform: "
+  show_menu
+  read choice
+  case $choice in
+    1) CreateContainer ;;
+    2) ConnectToGitRepo ;;
+    3) ManageContainer ;;
+    4) ContainerUsageReport ;;
+
+    *) echo "Invalid selection. Please try again." ;;
+  esac
+}
+
+
+CreateContainer(){
+    ./scripts/create_container.sh
+}
+CreateContainer(){
+    ./scripts/manage_containers.sh
+}
+CreateContainer(){
+    ./scripts/resource_report.sh
+}
+ConnectToGitRepo(){
+    ./scripts/git_deploy.sh
+
+}
